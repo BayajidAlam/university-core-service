1a455d8ad3d19da780dba6fe3b515ff725fbb45c
BayajidAlam <bayajidalam2001@gmail.com>
2023-09-19 20:57:26 +0600

    cloudinary and multer setup done

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -10,16 +10,15 @@ router.get('/', AcademicSemeterController.getAllFromDB);
 router.get('/:id', AcademicSemeterController.getDataById);
 router.post(
   '/',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.create),
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+//   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   AcademicSemeterController.insertIntoDB
 );
 
 router.patch(
   '/:id',
   validateRequest(AcademicSemesterValidation.update),
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AcademicSemeterController.updateOneInDB
 );
 
