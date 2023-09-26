347ee7db78e32e0ff6981728ba38f7a5813998cf
BayajidAlam <bayajidalam2001@gmail.com>
2023-09-26 20:25:48 +0600

    core service done

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import { Server } from 'http';
 import app from './app';
+import subscribeToEvents from './app/events';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
 import { RedisClient } from './shared/redis';
-import subscribeToEvents from './app/events';
 
 
 async function bootstrap() {
-  await RedisClient.connect().then(()=>{
+  await RedisClient.connect().then(() => {
     subscribeToEvents()
-  })
+  });
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
